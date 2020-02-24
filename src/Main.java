a8494ca01ce017e3d1e523a49ac99249f94104e5
DELL <singh.chirag.rana@gmail.com>
2020-02-24 08:44:42 -0500

    Print the menu.Take input from user to perform one operation from menu

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CHIRAG's Contact List App");
+        System.out.println("Press 1 to add a new contact");
+        System.out.println("Press 2 to view all contacts");
+        System.out.println("Press 3 to search for a contact");
+        System.out.println("Press 4 to delete a contact");
+        System.out.println("Press 5 to exit program");
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
+
+
+    }
+}
